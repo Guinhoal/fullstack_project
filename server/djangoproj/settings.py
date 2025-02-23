7bdffd1c39b9c821e341f5b82a48ea4dfd7540f4
Thiago Arthur <thiagodevprof@gmail.com>
2025-02-22 23:44:18 -0300

    lala

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,15 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost','http://localhost:8000/']
-CSRF_TRUSTED_ORIGINS=['http://localhost:8000/']
+ALLOWED_HOSTS = [
+    'localhost',
+    'thiagodevpro-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000/',
+    'https://thiagodevpro-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
